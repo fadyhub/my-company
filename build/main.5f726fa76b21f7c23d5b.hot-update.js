1fa5cd99dc6049b82c0a37e3680b76af62753797
fadyhub <PUT_YOUR_EMAIL_HERE>
2024-09-30 20:55:26 +0300

    first commit

---
+++ build/main.5f726fa76b21f7c23d5b.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatecompany"]("main",{
+
+/***/ "./src/sass/style.scss":
+/*!*****************************!*\
+  !*** ./src/sass/style.scss ***!
+  \*****************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nExpected identifier.\\n\\u001b[34m   ╷\\u001b[0m\\n\\u001b[34m82 │\\u001b[0m     color: $\\u001b[31m\\u001b[0m;\\r\\n\\u001b[34m   │\\u001b[0m \\u001b[31m            ^\\u001b[0m\\n\\u001b[34m   ╵\\u001b[0m\\n  src\\\\sass\\\\style.scss 82:13  root stylesheet\\n    at tryRunOrWebpackError (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:86:9)\\n    at __webpack_require_module__ (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5276:12)\\n    at __webpack_require__ (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5233:18)\\n    at C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5305:20\\n    at symbolIterator (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5211:43\\n    at symbolIterator (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n    at timesSync (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:2297:7)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nExpected identifier.\\n\\u001b[34m   ╷\\u001b[0m\\n\\u001b[34m82 │\\u001b[0m     color: $\\u001b[31m\\u001b[0m;\\r\\n\\u001b[34m   │\\u001b[0m \\u001b[31m            ^\\u001b[0m\\n\\u001b[34m   ╵\\u001b[0m\\n  src\\\\sass\\\\style.scss 82:13  root stylesheet\\n    at Object.<anonymous> (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\src\\\\sass\\\\style.scss:1:7)\\n    at C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:453:10\\n    at Hook.eval [as call] (eval at create (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5278:39\\n    at tryRunOrWebpackError (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:81:7)\\n    at __webpack_require_module__ (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5276:12)\\n    at __webpack_require__ (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5233:18)\\n    at C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5305:20\\n    at symbolIterator (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n\\nGenerated code for C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\AHMED\\\\OneDrive\\\\Desktop\\\\company\\\\src\\\\sass\\\\style.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nExpected identifier.\\\\n\\\\u001b[34m   ╷\\\\u001b[0m\\\\n\\\\u001b[34m82 │\\\\u001b[0m     color: $\\\\u001b[31m\\\\u001b[0m;\\\\r\\\\n\\\\u001b[34m   │\\\\u001b[0m \\\\u001b[31m            ^\\\\u001b[0m\\\\n\\\\u001b[34m   ╵\\\\u001b[0m\\\\n  src\\\\\\\\sass\\\\\\\\style.scss 82:13  root stylesheet\\\");\");\n\n//# sourceURL=webpack://company/./src/sass/style.scss?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("0e3f149126975b40a336")
+/******/ })();
+/******/ 
+/******/ }
+);
---
+++ build/main.ec9a4e4a409f570bef65.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatecompany"]("main",{
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: The keyword 'let' is reserved (24:4)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n|     if(pathname == \\\"/blog-details.html\\\" || pathname == \\\"/add-blog.html\\\")\\n| \\n>     let modalId = $('#image-gallery');\\n| \\n|     loadGallery(true, 'a.thumbnail');\");\n\n//# sourceURL=webpack://company/./src/js/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("cff07f0918f430f1507a")
+/******/ })();
+/******/ 
+/******/ }
+);
